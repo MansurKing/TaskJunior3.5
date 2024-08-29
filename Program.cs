cab1f08c4b1d778974ebcb527ba7a5ed900fe5d9
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-29 20:13:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TaskJunior3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int firstValueRandom = 0;
+            int finishValueRandom = 10;
+
+            int numberOfNumbers = 30;
+            int[] numbers = new int[numberOfNumbers];
+            int lastNumberIndex = numbers.Length - 1;
+
+            int baseNumbersInARow = 1;
+            int numbersInARow = 1;
+            int recurringNumber = 0;
+
+
+            Console.WriteLine(numbers.Length + " - кол-во элементов в самом массиве.\n\nМассив:\n");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(firstValueRandom, finishValueRandom);
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine("\n\nПоиск повторяющихся чисел:\n");
+
+            for (int i = 0; i < lastNumberIndex; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbersInARow++;
+
+                    if (numbersInARow >= baseNumbersInARow)
+                    {
+                        recurringNumber = numbers[i];
+                        baseNumbersInARow = numbersInARow;
+                    }
+                    else
+                    {
+                        numbersInARow = 1;
+                    }
+                }
+            }
+
+            if (baseNumbersInARow > 1)
+            {
+                Console.WriteLine($"\nЧисло {recurringNumber} повторяется {baseNumbersInARow} раз/а подряд.");
+            }
+            else
+            {
+                Console.WriteLine("\nНет повторяющихся подряд чисел.");
+            }
+        }
+    }
+}
+
+
